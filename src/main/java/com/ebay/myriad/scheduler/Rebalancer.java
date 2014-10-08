54d96b78c69ff829896a129854e911e01fbd36aa
Mohit Soni <mohitsoni1989@gmail.com>
2014-10-08 11:50:40 -0700

    Removed unused YARN REST Client

--- src/main/java/com/ebay/myriad/scheduler/Rebalancer.java
+++ src/main/java/com/ebay/myriad/scheduler/Rebalancer.java
@@ -15,9 +15,6 @@
  */
 package com.ebay.myriad.scheduler;
 
-import com.ebay.myriad.client.yarn.AppsResponse;
-import com.ebay.myriad.client.yarn.ClusterMetrics;
-import com.ebay.myriad.client.yarn.YARNResourceManagerService;
 import com.ebay.myriad.configuration.MyriadConfiguration;
 import com.ebay.myriad.state.Cluster;
 import com.ebay.myriad.state.SchedulerState;
@@ -25,8 +22,6 @@ import com.google.common.base.Preconditions;
 import org.apache.commons.collections.MapUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import retrofit.RestAdapter;
-import retrofit.RestAdapter.LogLevel;
 
 import javax.inject.Inject;
 import java.util.Map;
@@ -81,52 +76,52 @@ public class Rebalancer implements Runnable {
             String port = cluster.getResourceManagerPort();
             double minQuota = cluster.getMinQuota();
 
-            RestAdapter restAdapter = new RestAdapter.Builder()
-                    .setEndpoint("http://" + host + ":" + port)
-                    .setLogLevel(LogLevel.FULL).build();
-            YARNResourceManagerService service = restAdapter
-                    .create(YARNResourceManagerService.class);
+//            RestAdapter restAdapter = new RestAdapter.Builder()
+//                    .setEndpoint("http://" + host + ":" + port)
+//                    .setLogLevel(LogLevel.FULL).build();
+//            YARNResourceManagerService service = restAdapter
+//                    .create(YARNResourceManagerService.class);
+//
+//            ClusterMetrics metrics = service.metrics().getClusterMetrics();
+//            AppsResponse appsResponse = service.apps("ACCEPTED");
+//
+//            int acceptedApps = 0;
+//
+//            if (appsResponse == null || appsResponse.getApps() == null
+//                    || appsResponse.getApps().getApps() == null) {
+//                acceptedApps = 0;
+//            } else {
+//                acceptedApps = appsResponse.getApps().getApps().size();
+//            }
+//            LOGGER.info("Metrics: {}", metrics);
+//            LOGGER.info("Apps: {}", appsResponse);
+//
+//            long availableMB = metrics.getAvailableMB();
+//            long allocatedMB = metrics.getAllocatedMB();
+//            long reservedMB = metrics.getReservedMB();
+//            int activeNodes = metrics.getActiveNodes();
+//            int unhealthyNodes = metrics.getUnhealthyNodes();
+//            int appsPending = metrics.getAppsPending();
+//            int appsRunning = metrics.getAppsRunning();
 
-            ClusterMetrics metrics = service.metrics().getClusterMetrics();
-            AppsResponse appsResponse = service.apps("ACCEPTED");
-
-            int acceptedApps = 0;
-
-            if (appsResponse == null || appsResponse.getApps() == null
-                    || appsResponse.getApps().getApps() == null) {
-                acceptedApps = 0;
-            } else {
-                acceptedApps = appsResponse.getApps().getApps().size();
-            }
-            LOGGER.info("Metrics: {}", metrics);
-            LOGGER.info("Apps: {}", appsResponse);
-
-            long availableMB = metrics.getAvailableMB();
-            long allocatedMB = metrics.getAllocatedMB();
-            long reservedMB = metrics.getReservedMB();
-            int activeNodes = metrics.getActiveNodes();
-            int unhealthyNodes = metrics.getUnhealthyNodes();
-            int appsPending = metrics.getAppsPending();
-            int appsRunning = metrics.getAppsRunning();
-
-            if (activeNodes == 0 && appsPending > 0) {
-                LOGGER.info(
-                        "Flexing up for condition: activeNodes ({}) == 0 && appsPending ({}) > 0",
-                        activeNodes, appsPending);
-                this.myriadOperations.flexUpCluster(clusterId, 1, "small");
-            } else if (appsPending == 0 && appsRunning == 0 && activeNodes > 0) {
-                LOGGER.info(
-                        "Flexing down for condition: appsPending ({}) == 0 && appsRunning ({}) == 0 && activeNodes ({}) > 0",
-                        appsPending, appsRunning, activeNodes);
-                this.myriadOperations.flexDownCluster(cluster, 1);
-            } else if (acceptedApps > 0) {
-                LOGGER.info("Flexing up for condition: acceptedApps ({}) > 0",
-                        acceptedApps);
-                this.myriadOperations.flexUpCluster(clusterId, 1, "small");
-            } else {
-                LOGGER.info("Nothing to rebalance");
-                this.schedulerState.releaseLock(clusterId);
-            }
+//            if (activeNodes == 0 && appsPending > 0) {
+//                LOGGER.info(
+//                        "Flexing up for condition: activeNodes ({}) == 0 && appsPending ({}) > 0",
+//                        activeNodes, appsPending);
+//                this.myriadOperations.flexUpCluster(clusterId, 1, "small");
+//            } else if (appsPending == 0 && appsRunning == 0 && activeNodes > 0) {
+//                LOGGER.info(
+//                        "Flexing down for condition: appsPending ({}) == 0 && appsRunning ({}) == 0 && activeNodes ({}) > 0",
+//                        appsPending, appsRunning, activeNodes);
+//                this.myriadOperations.flexDownCluster(cluster, 1);
+//            } else if (acceptedApps > 0) {
+//                LOGGER.info("Flexing up for condition: acceptedApps ({}) > 0",
+//                        acceptedApps);
+//                this.myriadOperations.flexUpCluster(clusterId, 1, "small");
+//            } else {
+//                LOGGER.info("Nothing to rebalance");
+//                this.schedulerState.releaseLock(clusterId);
+//            }
         }
     }
 }
