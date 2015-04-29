56cb56b95c2d5b22da49934c5cc6f38d42031874
Brandon Gulla <brandon@brandongulla.com>
2015-04-29 15:28:26 -0400

    changed HADOOP_INSTALL env to HADOOP_HOME

--- docker/myriad-bin/install-yarn.sh
+++ docker/myriad-bin/install-yarn.sh
@@ -10,7 +10,7 @@ HADOOP_VER=2.5.2
 # Put in env defaults if they are missing
 export HADOOP_GROUP=${HADOOP_GROUP:='hadoop'}
 export HADOOP_USER=${HADOOP_USER:='hduser'}
-export HADOOP_INSTALL=${HADOOP_INSTALL:='/usr/local/hadoop'}
+export HADOOP_HOME=${HADOOP_HOME:='/usr/local/hadoop'}
 
 
 # Add hduser user
@@ -21,23 +21,23 @@ chown -R $HADOOP_USER:$HADOOP_GROUP /home/${HADOOP_USER}
 
 # Extract Hadoop
 tar vxzf hadoop-${HADOOP_VER}.tar.gz -C /tmp
-mv /tmp/hadoop-${HADOOP_VER} ${HADOOP_INSTALL}
-chown -R ${HADOOP_USER}:${HADOOP_GROUP} ${HADOOP_INSTALL}
+mv /tmp/hadoop-${HADOOP_VER} ${HADOOP_HOME}
+chown -R ${HADOOP_USER}:${HADOOP_GROUP} ${HADOOP_HOME}
 
 # Init bashrc with hadoop env variables
 sh -c 'echo export JAVA_HOME=/usr >> /home/${HADOOP_USER}/.bashrc'
-sh -c 'echo export HADOOP_INSTALL=\${HADOOP_INSTALL} >> /home/${HADOOP_USER}/.bashrc'
-sh -c 'echo export PATH=\$PATH:\${HADOOP_INSTALL}/bin >> /home/${HADOOP_USER}/.bashrc'
-sh -c 'echo export PATH=\$PATH:\${HADOOP_INSTALL}/sbin >> /home/${HADOOP_USER}/.bashrc'
-sh -c 'echo export HADOOP_MAPRED_HOME=\${HADOOP_INSTALL} >> /home/${HADOOP_USER}/.bashrc'
-sh -c 'echo export HADOOP_COMMON_HOME=\${HADOOP_INSTALL} >> /home/${HADOOP_USER}/.bashrc'
-sh -c 'echo export HADOOP_HDFS_HOME=\${HADOOP_INSTALL} >> /home/${HADOOP_USER}/.bashrc'
-sh -c 'echo export YARN_HOME=\${HADOOP_INSTALL} >> /home/${HADOOP_USER}/.bashrc'
-sh -c 'echo export HADOOP_COMMON_LIB_NATIVE_DIR=\$\{HADOOP_INSTALL\}/lib/native >> /home/${HADOOP_USER}/.bashrc'
-sh -c 'echo export HADOOP_OPTS=\"-Djava.library.path=\${HADOOP_INSTALL}/lib\" >> /home/${HADOOP_USER}/.bashrc'
+sh -c 'echo export HADOOP_HOME=\${HADOOP_HOME} >> /home/${HADOOP_USER}/.bashrc'
+sh -c 'echo export PATH=\$PATH:\${HADOOP_HOME}/bin >> /home/${HADOOP_USER}/.bashrc'
+sh -c 'echo export PATH=\$PATH:\${HADOOP_HOME}/sbin >> /home/${HADOOP_USER}/.bashrc'
+sh -c 'echo export HADOOP_MAPRED_HOME=\${HADOOP_HOME} >> /home/${HADOOP_USER}/.bashrc'
+sh -c 'echo export HADOOP_COMMON_HOME=\${HADOOP_HOME} >> /home/${HADOOP_USER}/.bashrc'
+sh -c 'echo export HADOOP_HDFS_HOME=\${HADOOP_HOME} >> /home/${HADOOP_USER}/.bashrc'
+sh -c 'echo export YARN_HOME=\${HADOOP_HOME} >> /home/${HADOOP_USER}/.bashrc'
+sh -c 'echo export HADOOP_COMMON_LIB_NATIVE_DIR=\$\{HADOOP_HOME\}/lib/native >> /home/${HADOOP_USER}/.bashrc'
+sh -c 'echo export HADOOP_OPTS=\"-Djava.library.path=\${HADOOP_HOME}/lib\" >> /home/${HADOOP_USER}/.bashrc'
 
 
 # Link Mesos Libraries
-echo "export MESOS_NATIVE_JAVA_LIBRARY=/usr/local/lib/libmesos.so" >> ${HADOOP_INSTALL}/etc/hadoop/hadoop-env.sh
+echo "export MESOS_NATIVE_JAVA_LIBRARY=/usr/local/lib/libmesos.so" >> ${HADOOP_HOME}/etc/hadoop/hadoop-env.sh
 
 echo "end of install-yarn.sh script"
