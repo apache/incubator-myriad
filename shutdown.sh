cc1996988593eae9fdd4d167c8410695cd5d2681
Sarjeet Singh <ssarjeetsingh@gmail.com>
2015-07-14 16:57:24 -0700

    Minor comment typos in shutdown.sh script.
    Typo in comments, It Should be 'Stop' instead of 'Start'. Thanks.

--- shutdown.sh
+++ shutdown.sh
@@ -1,10 +1,10 @@
 set -e
 
-# Start HistoryServer
+# Stop HistoryServer
 sudo -u hduser sh -c '/usr/local/hadoop/sbin/mr-jobhistory-daemon.sh stop historyserver'
 
-# Start DataNode
+# Stop DataNode
 sudo -u hduser sh -c '/usr/local/hadoop/sbin/hadoop-daemons.sh stop datanode'
 
-# Start NameNode
+# Stop NameNode
 sudo -u hduser sh -c '/usr/local/hadoop/sbin/hadoop-daemon.sh stop namenode'
