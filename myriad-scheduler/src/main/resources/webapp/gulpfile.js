ddfe72e20767191d6a6f1c1364412d658580f0d1
klucar <klucar@gmail.com>
2015-10-21 12:51:49 -0400

    Fixes MYRIAD-114
    Fixed gulp build to not include local paths and gradle-ized build.
    Removed public/ from git repo and added it to .gitignore.

--- myriad-scheduler/src/main/resources/webapp/gulpfile.js
+++ myriad-scheduler/src/main/resources/webapp/gulpfile.js
@@ -1,82 +1,60 @@
 'use strict';
 
-var browserify = require('browserify');
 var gulp = require('gulp');
+
 var transform = require('vinyl-transform');
 var source = require('vinyl-source-stream');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
-var rename = require('gulp-rename');
-var reactify = require('reactify');
-var watchify = require('watchify');
-var webserver = require('gulp-webserver');
 var buffer = require('vinyl-buffer');
+var watchify = require('watchify');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var webserver = require('gulp-webserver');
+var del = require('del');
 
-
-gulp.task('webserver', function() {
-    gulp.src('app')
-    .pipe(webserver({
-        livereload: true,
-        directoryListing: true,
-        open: true
-    }));
-});
-
-var getBundler = function () {
-    
-    return browserify({
+gulp.task("js", ['clean'], function () {
+  browserify({
     entries: ['./js/app.js'], // Only need initial file, browserify finds the deps
-    transform: [reactify], // We want to convert JSX to normal javascript
-    debug: true, // Gives us sourcemapping
-    cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
-})};
-
-gulp.task('javascript', function () {
-
-  var b = getBundler();
-  
-  return b.bundle()
-    .pipe(source('./js/app.js'))
-    .pipe(buffer())
-    //.pipe(uglify())
+    transform: ['babelify'],
+    debug: false,
+    fullPaths: false
+    })
+    .bundle()
+    .pipe(source('bundle.js'))
     .pipe(rename('myriad.js'))
-    .pipe(gulp.dest('./public/js'));
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest("public/js/"));
 });
 
-gulp.task('html', function () {
+gulp.task("js-dev", ['clean'], function () {
+  browserify({
+    entries: ['./js/app.js'], // Only need initial file, browserify finds the deps
+    transform: ['babelify'],
+    debug: true,
+    fullPaths: true
+    })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(rename('myriad.js'))
+    .pipe(gulp.dest("public/js/"));
+});
+
+
+gulp.task('html', ['clean'], function () {
     return gulp.src('*.html')
         .pipe( gulp.dest('public/'))
 });
 
-gulp.task('css', function () {
+gulp.task('css', ['clean'], function () {
     return gulp.src('css/*.css')
         .pipe( gulp.dest('public/css/'))
 });
 
-gulp.task('img', function () {
+gulp.task('img', ['clean'], function () {
     return gulp.src('img/**')
         .pipe( gulp.dest('public/img/'))
-});
-
-gulp.task('dev-js', function () {
- 
-    var watcher  = watchify(getBundler());
-
-    return watcher
-    .on('update', function () { // When any files update
-        var updateStart = Date.now();
-        console.log('Updating!');
-        watcher.bundle() // Create new bundle that uses the cache for high performance
-        .pipe(source('app.js'))
-    // This is where you add uglifying etc.
-        .pipe(gulp.dest('./public/js/'));
-        console.log('Updated!', (Date.now() - updateStart) + 'ms');
-    })
-    .bundle() // Create the initial bundle when starting the task
-    .pipe(source('app.js'))
-    .pipe(rename('myriad.js'))
-    .pipe(gulp.dest('./public/js/'));
-
 });
 
 gulp.task('webserver', function() {
@@ -92,11 +70,16 @@ gulp.task('webserver', function() {
 gulp.task('watch', function() {
     gulp.watch('index.html', ['html']);
     gulp.watch('css/**', ['css']);
-    gulp.watch('js/**', ['javascript']);
+    gulp.watch('js/**', ['js']);
     gulp.watch('img/**', ['img']);
 });
 
-gulp.task('dev', ['default', 'watch', 'webserver']);
+gulp.task('clean', function() {
+  return del(['./public']);
+});
 
-gulp.task('default', ['javascript', 'html', 'css', 'img']);
+gulp.task('dev', ['js-dev', 'html', 'css', 'img', 'watch', 'webserver']);
 
+gulp.task('default', ['js', 'html', 'css', 'img']);
+
+gulp.task('build', ['default']); // gradle calls gulp build by default
