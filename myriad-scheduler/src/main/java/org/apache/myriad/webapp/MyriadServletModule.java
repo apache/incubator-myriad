pdread100/myriad

# This is a combination of 2 commits. # The first commit's message is:
rebase, removed all but one shutdown

# This is the 2nd commit message:

Remove all but one destroy menu option

myriad-scheduler/src/main/java/com/ebay/myriad/webapp/MyriadServletModule.java
@@ -3,6 +3,7 @@ package com.ebay.myriad.webapp;
 import com.ebay.myriad.api.ClustersResource;
 import com.ebay.myriad.api.ConfigurationResource;
 import com.ebay.myriad.api.SchedulerStateResource;
+import com.ebay.myriad.api.ControllerResource;
 import com.google.inject.Scopes;
 import com.google.inject.servlet.ServletModule;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
@@ -18,6 +19,7 @@ public class MyriadServletModule extends ServletModule {
         bind(ClustersResource.class);
         bind(ConfigurationResource.class);
         bind(SchedulerStateResource.class);
+        bind(ControllerResource.class);
 
         bind(GuiceContainer.class);
         bind(JacksonJaxbJsonProvider.class).in(Scopes.SINGLETON);

apache/incubator-myriad

myriad-scheduler/src/main/java/org/apache/myriad/webapp/MyriadServletModule.java
@@ -18,7 +18,9 @@
  */
 package org.apache.myriad.webapp;
 
+import org.apache.myriad.api.ClustersResource;
 import org.apache.myriad.api.ConfigurationResource;
+import org.apache.myriad.api.ControllerResource;
 import org.apache.myriad.api.SchedulerStateResource;
 import com.google.inject.Scopes;
 import com.google.inject.servlet.ServletModule;
@@ -32,9 +34,10 @@ public class MyriadServletModule extends ServletModule {
 
   @Override
   protected void configureServlets() {
-    bind(org.apache.myriad.api.ClustersResource.class);
+    bind(ClustersResource.class);
     bind(ConfigurationResource.class);
     bind(SchedulerStateResource.class);
+    bind(ControllerResource.class);
 
     bind(GuiceContainer.class);
     bind(JacksonJaxbJsonProvider.class).in(Scopes.SINGLETON);