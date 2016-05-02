bcd1fc8c6f6ea184522bc9a2c58610710a4be9b3
darinj <darinj.work@gmail.com>
2016-05-02 15:08:20 -0400

    Added Docker Support, see the README.md in docker directory for quick start guide.
    Code changes:
    - Added classes necessary to pass configuration to TaskFactory
    - Modified classes necessary to consume the configuration changes
    - Edits to docker/scripts mostly to break up the build
    
    @Todo
    - add support for MesosInfo (for AppC support), will be done as more information about the MesosInfo object appears.
    - add configuration Uri to avoid rebuilding images (done, separate PR).
    - clean up docker scripts more, including the ability to pass build arguments, will be incrementally done.
    
    JIRA:
      [Myriad-136] https://issues.apache.org/jira/browse/MYRIAD-136
    Pull Request:
      Closes #67
    Author:
      DarinJ <darinj@apache.org>
    
    Code changes:
    - Added classes necessary to pass configuration to TaskFactory
    - Modified classes necessary to consume the configuration changes
    - Edits to docker/scripts mostly to break up the build
    
    @Todo
    - add support for MesosInfo (for AppC support), will be done as more information about the MesosInfo object appears.
    - add configuration Uri to avoid rebuilding images (done, separate PR).
    - clean up docker scripts more, including the ability to pass build arguments, will be incrementally done.

--- myriad-scheduler/src/test/java/org/apache/myriad/scheduler/TestTaskUtils.java
+++ myriad-scheduler/src/test/java/org/apache/myriad/scheduler/TestTaskUtils.java
@@ -19,13 +19,17 @@ package org.apache.myriad.scheduler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.google.common.collect.Range;
+import com.google.common.collect.Ranges;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.apache.mesos.Protos;
 import org.apache.myriad.configuration.MyriadBadConfigurationException;
 import org.apache.myriad.configuration.MyriadConfiguration;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -35,12 +39,20 @@ import static org.junit.Assert.fail;
 public class TestTaskUtils {
 
   static MyriadConfiguration cfg;
+  static MyriadConfiguration cfgWithRole;
+  static MyriadConfiguration cfgWithDocker;
+  static double epsilon = .0001;
 
   @BeforeClass
   public static void setUpBeforeClass() throws Exception {
     ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
     cfg = mapper.readValue(Thread.currentThread().getContextClassLoader().getResource("myriad-config-test-default.yml"),
         MyriadConfiguration.class);
+    cfgWithRole = mapper.readValue(Thread.currentThread().getContextClassLoader().getResource("myriad-config-test-default-with-framework-role.yml"),
+        MyriadConfiguration.class);
+    cfgWithDocker = mapper.readValue(Thread.currentThread().getContextClassLoader().getResource("myriad-config-test-default-with-docker-info.yml"),
+            MyriadConfiguration.class);
+
   }
 
   @AfterClass
@@ -74,7 +86,6 @@ public class TestTaskUtils {
     Gson gson = new GsonBuilder().registerTypeAdapter(ServiceResourceProfile.class, new ServiceResourceProfile.CustomDeserializer())
         .create();
 
-
     ServiceResourceProfile parentProfile = new ServiceResourceProfile("abc", 1.0, 100.0);
 
     String parentStr = gson.toJson(parentProfile);
@@ -97,5 +108,86 @@ public class TestTaskUtils {
   public void testStackTrace() {
 
     new Throwable().printStackTrace();
+  }
+  private Protos.Offer createScalarOffer(String name, double roleVal, double defaultVal) {
+    Protos.Offer offer = Protos.Offer.newBuilder()
+        .setId(Protos.OfferID.newBuilder().setValue("offerId"))
+        .setSlaveId(Protos.SlaveID.newBuilder().setValue("slaveId"))
+        .setHostname("test.com")
+        .setFrameworkId(Protos.FrameworkID.newBuilder().setValue("frameworkId"))
+        .addResources(
+            Protos.Resource.newBuilder()
+                .setScalar(Protos.Value.Scalar.newBuilder().setValue(roleVal))
+                .setType(Protos.Value.Type.SCALAR)
+                .setName(name)
+                .setRole("test")
+                .build())
+        .addResources(
+            Protos.Resource.newBuilder()
+                .setScalar(Protos.Value.Scalar.newBuilder().setValue(defaultVal))
+                .setType(Protos.Value.Type.SCALAR)
+                .setName("cpus")
+                .build())
+        .build();
+    return offer;
+  }
+
+  private void checkResourceList(Iterable<Protos.Resource> resources, String name, Double roleVal, Double defaultVal) {
+    int i = 0;
+    Range defaultValueRange = Ranges.closed(defaultVal - epsilon, defaultVal + epsilon);
+    Range roleValueRange = Ranges.closed(roleVal - epsilon, roleVal + epsilon);
+
+    for (Protos.Resource resource: resources) {
+      if (resource.hasRole() && resource.getRole().equals("test")) {
+        double v = resource.getScalar().getValue();
+        assertTrue("Test Role  has " + v + " " + name + " should have " + roleVal, roleValueRange.contains(v));
+        i++;
+      } else {
+        double v = resource.getScalar().getValue();
+        assertTrue("Default Role has " + v + " " + name + " should have " + defaultVal , defaultValueRange.contains(v));
+        i++;
+      }
+    }
+    assertTrue("There should be at most 2 resources", i <= 2);
+  }
+
+  @Test
+  public void testGetScalarResourcesWithRole() {
+    TaskUtils taskUtils = new TaskUtils(cfgWithRole);
+    checkResourceList(taskUtils.getScalarResource(createScalarOffer("cpus", 3.0, 2.0), "cpus", 1.0, 0.0), "cpus", 1.0, 0.0);
+    checkResourceList(taskUtils.getScalarResource(createScalarOffer("cpus", 0.0, 2.0), "cpus", 1.0, 1.0), "cpus", 0.0, 1.0);
+    checkResourceList(taskUtils.getScalarResource(createScalarOffer("cpus", 1.5, 2.0), "cpus", 2.0, 1.0), "cpus", 0.5, 1.5);
+    checkResourceList(taskUtils.getScalarResource(createScalarOffer("cpus", 1.5, 2.0), "cpus", 1.5, 2.0), "cpus", 0.0, 1.5);
+  }
+
+  @Test
+  public void testGetScalarResources() {
+    TaskUtils taskUtils = new TaskUtils(cfg);
+    checkResourceList(taskUtils.getScalarResource(createScalarOffer("cpus", 0.0, 2.0), "cpus", 1.0, 0.0), "cpus", 0.0, 1.0);
+    checkResourceList(taskUtils.getScalarResource(createScalarOffer("cpus", 0.0, 2.0), "cpus", 1.0, 1.0), "cpus", 0.0, 1.0);
+    checkResourceList(taskUtils.getScalarResource(createScalarOffer("cpus", 0.0, 2.0), "cpus", 1.0, 1.0), "cpus", 0.0, 1.0);
+    checkResourceList(taskUtils.getScalarResource(createScalarOffer("cpus", 0.0, 2.0), "cpus", 0.5, 1.5), "cpus", 0.0, 0.5);
+  }
+
+  @Test
+  public void testContainerInfo() {
+    TaskUtils taskUtils = new TaskUtils(cfgWithDocker);
+    Protos.ContainerInfo containerInfo = taskUtils.getContainerInfo();
+    assertTrue("The container should have a docker", containerInfo.hasDocker());
+    assertTrue("There should be two volumes", containerInfo.getVolumesCount() == 2);
+    assertTrue("The first volume should be read only", containerInfo.getVolumes(0).getMode().equals(Protos.Volume.Mode.RO));
+    assertTrue("The first volume should be read write", containerInfo.getVolumes(1).getMode().equals(Protos.Volume.Mode.RW));
+  }
+
+  @Test public void testDockerInfo() {
+    TaskUtils taskUtils = new TaskUtils(cfgWithDocker);
+    Protos.ContainerInfo containerInfo = taskUtils.getContainerInfo();
+    assertTrue("The container should have a docker", containerInfo.hasDocker());
+    assertTrue("There should be two volumes", containerInfo.getVolumesList().size() == 2);
+    assertTrue("There should be a docker image", containerInfo.getDocker().hasImage());
+    assertTrue("The docker image should be mesos/myraid", containerInfo.getDocker().getImage().equals("mesos/myriad"));
+    assertTrue("Should be using host networking", containerInfo.getDocker().getNetwork().equals(Protos.ContainerInfo.DockerInfo.Network.HOST));
+    assertTrue("There should be two parameters", containerInfo.getDocker().getParametersList().size() == 2);
+    assertTrue("Privledged mode should be false", containerInfo.getDocker().getPrivileged() == false);
   }
 }
